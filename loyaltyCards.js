8324168fbf1845c611b08a6a2246e5c6be725e8f
edenramoneda <edenlee143@gmail.com>
2025-07-02 23:34:14 +0800

    Fixed the fetching of loyalty cards

--- loyaltyCards.js
+++ loyaltyCards.js
@@ -3,6 +3,7 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
   DynamoDBDocumentClient,
   GetCommand,
+  ScanCommand,
   PutCommand,
 } = require("@aws-sdk/lib-dynamodb");
 
@@ -18,29 +19,29 @@ const docClient = DynamoDBDocumentClient.from(client);
 app.use(express.json());
 
 app.get("/loyalty-cards", async (req, res) => {
-    const params = {
-      TableName: LOYALTY_CARDS_TABLE
-    };
-  
-    try {
-        const command = new ScanCommand(params);
-        const { Items } = await docClient.send(command);
-        
-        if (Items && Items.length > 0) {
-          const coupons = Items.map(item => ({
-            loyaltyCardId: item.loyaltyCardId,
-            name: item.name
-          }));
-          res.json({ coupons });
-        } else {
-          res.json({ coupons: [] });
-        }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Could not retrieve loyalty cards " . error});
-    }
-  });
+  const params = {
+    TableName: LOYALTY_CARDS_TABLE
+  };
 
+  try {
+      const command = new ScanCommand(params);
+      const { Items } = await docClient.send(command);
+      
+      if (Items && Items.length > 0) {
+        const coupons = Items.map(item => ({
+          loyaltyCardId: item.loyaltyCardId,
+          name: item.name
+        }));
+        res.json({ coupons });
+      } else {
+        res.json({ coupons: [] });
+      }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retrieve loyalty cards " . error});
+  }
+});
+  
 
 app.post("/loyalty-card", async (req, res) => {
     const { loyaltyCardId, name } = req.body;
