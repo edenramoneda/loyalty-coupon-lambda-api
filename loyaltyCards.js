fce6b7ab87931ac3c0e1a611ba1e49b26de00d87
edenramoneda <edenlee143@gmail.com>
2025-07-03 22:41:58 +0800

    Added the fetching of data by coupon Id

--- loyaltyCards.js
+++ loyaltyCards.js
@@ -30,7 +30,12 @@ app.get("/loyalty-cards", async (req, res) => {
       if (Items && Items.length > 0) {
         const loyaltyCards = Items.map(item => ({
           loyaltyCardId: item.loyaltyCardId,
-          name: item.name
+          customerName: item.customerName,
+          email: item.email,
+          pointsBalance: item.pointsBalance,
+          tier: item.tier,
+          signUpDate: item.signUpDate
+
         }));
         res.json({ loyaltyCards });
       } else {
@@ -44,28 +49,60 @@ app.get("/loyalty-cards", async (req, res) => {
   
 
 app.post("/loyalty-card", async (req, res) => {
-    const { loyaltyCardId, name } = req.body;
-    if (typeof loyaltyCardId !== "string") {
-       res.status(400).json({ error: '"loyaltyCardId" must be a string' });
-    } else if (typeof name !== "string") {
-       res.status(400).json({ error: '"name" must be a string' });
-    }
+    const { 
+      loyaltyCardId, 
+      customerName,
+      email,
+      pointsBalance,
+      tier,
+      signUpDate 
+    } = req.body;
+    // if (typeof loyaltyCardId !== "string") {
+    //    res.status(400).json({ error: '"loyaltyCardId" must be a string' });
+    // } else if (typeof name !== "string") {
+    //    res.status(400).json({ error: '"name" must be a string' });
+    // }
   
     const params = {
       TableName: LOYALTY_CARDS_TABLE,
-      Item: { loyaltyCardId, name },
+      Item: { loyaltyCardId, customerName,email,pointsBalance,tier,signUpDate },
     };
   
     try {
       const command = new PutCommand(params);
       await docClient.send(command);
-      res.json({ loyaltyCardId, name });
+      res.json({ loyaltyCardId, customerName, email, pointsBalance, tier, signUpDate });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Could not create loyalty card" });
+      res.status(500).json({ error: "Could not create loyalty card " + error});
     }
   });
   
+
+app.get("/loyalty-card/:loyaltyCardId", async (req, res) => {
+  const params = {
+    TableName: LOYALTY_CARDS_TABLE,
+    Key: {
+      loyaltyCardId: req.params.loyaltyCardId,
+    },
+  };
+
+  try {
+    const command = new GetCommand(params);
+    const { Item } = await docClient.send(command);
+    if (Item) {
+      const { loyaltyCardId, customerName, email, pointsBalance, tier, signUpDate } = Item;
+      res.json({ loyaltyCardId, customerName, email, pointsBalance, tier, signUpDate });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find loyalty card with provided "loyaltyCardId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retrieve loyalty card" });
+  }
+});
 
 app.use((req, res, next) => {
     return res.status(404).json({
